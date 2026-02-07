0fd114214710d407b330032393cd23c2fab345c9
Nizamuddin1N <ni43546376@gmail.com>
2026-02-08 02:01:02 +0530

    starting project

---
+++ apps/backend/services/order-service/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    service: 'auth',
+    time: new Date().toISOString()
+  });
+});
+
+const PORT = 4004;
+app.listen(PORT, () => {
+  console.log(`Auth Service running on port ${PORT}`);
+});
