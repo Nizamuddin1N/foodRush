e43cbd4cc15e3348b077e4dff4048199b460ec3d
Nizamuddin1N <nizamuddin00128@gmail.com>
2026-02-28 23:31:55 +0530

    solve proxy error

--- apps/backend/api-gateway/src/routes.js
+++ apps/backend/api-gateway/src/routes.js
@@ -3,25 +3,40 @@ import {createProxyMiddleware} from 'http-proxy-middleware';
 export const setupRoutes = (app)=>{
     app.use('/auth', createProxyMiddleware({
         target:process.env.AUTH_SERVICE,
-        changeOrigin: true
+        changeOrigin: true,
+        pathRewrite: {
+            '^/auth': ''
+        }
     }));
     
     app.use('/users', createProxyMiddleware({
         target:process.env.USER_SERVICE,
-        changeOrigin:true
+        changeOrigin:true,
+        pathRewrite: {
+            '^/users': ''
+        }
     }));
     
     app.use('/restaurants', createProxyMiddleware({
         target:process.env.RESTAURANT_SERVICE,
-        changeOrigin: true
+        changeOrigin: true,
+        pathRewrite: {
+            '^/restaurants': ''
+        }
     }))
     app.use('/orders', createProxyMiddleware({
         target: process.env.ORDER_SERVICE,
-        changeOrigin: true
+        changeOrigin: true,
+        pathRewrite: {
+            '^/orders': ''
+        }
     }));
     
     app.use('/payments', createProxyMiddleware({
         target: process.env.PAYMENT_SERVICE,
-        changeOrigin: true
+        changeOrigin: true,
+        pathRewrite: {
+            '^/payments': ''
+        }
     }));
 };
