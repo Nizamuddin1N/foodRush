084d8e030644d4393e15a5b375dfb0140e71e21f
Nizamuddin1N <nizamuddin00128@gmail.com>
2026-03-16 04:34:49 +0530

    add frontend URL

--- apps/backend/api-gateway/src/index.js
+++ apps/backend/api-gateway/src/index.js
@@ -8,18 +8,20 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: "*",
+  origin: [
+    "http://localhost:5173",
+    "https://foodrush-nizamuddin1ns-projects.vercel.app/"
+  ],
   credentials: true
 }))
 
+app.use(express.json())
 
 app.get('/health', (req, res) => {
   res.json({ status: "API Gateway running" })
 })
 
 setupRoutes(app)
-
-app.use(express.json())
 
 const PORT = Number(process.env.PORT) || 4000
 
